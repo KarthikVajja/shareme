2d430a645f64f60b9fab1c582b3e5afa2fc802b7
KarthikVajja <karthik31032000@gmail.com>
2023-01-22 11:57:41 -0500

    create all schemas

--- shareme_backend/schemas/index.js
+++ shareme_backend/schemas/index.js
@@ -1,2 +1,6 @@
 import user from './user';
-export const schemaTypes = [user]
+import pin from './pin.js'
+import save from './save.js'
+import comment from './comment'
+import postedBy from './postedBy';
+export const schemaTypes = [user, pin, save, comment, postedBy]
