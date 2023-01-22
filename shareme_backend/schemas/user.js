bbf2326fb23679880099c14c66581a642ddd7673
KarthikVajja <karthik31032000@gmail.com>
2023-01-22 11:46:38 -0500

    First schema

---
+++ shareme_backend/schemas/user.js
@@ -0,0 +1,16 @@
+export default{
+    name: 'user',
+    type: 'document',
+    fields: [
+        {
+            name: 'userName',
+            title: 'UserName',
+            type: 'string'
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'string'
+        },
+    ]
+}
